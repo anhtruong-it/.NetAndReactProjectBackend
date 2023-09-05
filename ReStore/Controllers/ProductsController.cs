2227ba273fde14cb1135acb53006d29c2d69c759
Anh Truong Nguyen <truongnguyen4042@gmail.com>
2023-09-06 00:09:47 +1000

    Add project files.

---
+++ ReStore/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReStore.Data;
+using ReStore.Entities;
+
+namespace ReStore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet] // api/products
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")] // api/products/id
+        public async Task<ActionResult<Product>> GetProduct(int id) 
+        {
+            return await context.Products.FindAsync(id);
+        }
+
+    }
+}
---
+++ ReStore/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReStore.Entities;
+
+namespace ReStore.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<Product> Products { get; set; } // create table products
+
+    }
+}
